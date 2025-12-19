b45fcfaefaa8813da897b7f16f3b215e8a5202dd
0x4973616163 <229308316+commit2main@users.noreply.github.com>
2025-12-19 11:16:04 +0000

    fix: drop into interactive fish shell

--- files/system/etc/profile.d/99-fish.sh
+++ files/system/etc/profile.d/99-fish.sh
@@ -1,10 +1,7 @@
 # From https://wiki.archlinux.org/title/Fish#Modify_.bashrc_to_drop_into_fish
 # Added interactivity check to workaround SDDM bug https://github.com/sddm/sddm/issues/1807
-if [[ $- == *i* ]] &&
-   [[ -z ${BASH_EXECUTION_STRING} ]] &&
-   [[ ${SHLVL} -eq 1 ]] &&
-   [[ $(ps -p $PPID -o comm=) != fish ]]
+if [[ $(ps --no-header --pid=$PPID --format=comm) != "fish" && -z ${BASH_EXECUTION_STRING} && ${SHLVL} -lt 3 ]]
 then
-    shopt -q login_shell && LOGIN_OPT='--login' || LOGIN_OPT=''
-    exec fish $LOGIN_OPT
+	shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
+	exec fish $LOGIN_OPTION
 fi
