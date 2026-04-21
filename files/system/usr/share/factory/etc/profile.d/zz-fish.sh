70cefd9275186f37553f78d18a6dfcb92d765528
commit2main <229308316+commit2main@users.noreply.github.com>
2026-04-21 18:53:24 +0800

    fix: remove zz-fish profile.d as its causing too many issues

--- files/system/usr/share/factory/etc/profile.d/zz-fish.sh
+++
@@ -1,7 +0,0 @@
-# From https://wiki.archlinux.org/title/Fish#Modify_.bashrc_to_drop_into_fish
-# Added interactivity check to workaround SDDM bug https://github.com/sddm/sddm/issues/1807
-if grep -qv 'fish' /proc/$PPID/comm && [[ ${SHLVL} == [1,2] ]] && [ -z "$GREETD_SOCK" ]
-then
-	shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=''
-	exec fish $LOGIN_OPTION
-fi
